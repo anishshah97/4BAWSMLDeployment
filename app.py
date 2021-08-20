d85fa48dec2091202d9db7a5272767754179ca39
Anish Shah <anish.shah.emails@gmail.com>
2021-08-20 15:52:14 -0400

    Update app.py
    Reformat to match Pep8 and Flake

--- app.py
+++ app.py
@@ -1,42 +1,46 @@
-from flask import Flask,request, url_for, redirect, render_template, jsonify
-import pandas as pd
-import pickle
-import numpy as np
 import os
+import pickle
+
+import numpy as np
+import pandas as pd
+from flask import Flask, jsonify, redirect, render_template, request, url_for
 
 app = Flask(__name__)
-model=pickle.load(open('treemodel.pkl','rb'))
+model = pickle.load(open('treemodel.pkl', 'rb'))
+
 
 @app.route('/')
 def home():
     return render_template("home.html")
 
 
-@app.route('/predict',methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
-    int_features=[x for x in request.form.values()]
-    final=np.array(int_features)
+    int_features = [x for x in request.form.values()]
+    final = np.array(int_features)
     col = ['age', 'sex', 'bmi', 'children', 'smoker', 'region']
-    data_unseen = pd.DataFrame([final], columns = col)
-    input_data=data_unseen[['age','bmi']].values
+    data_unseen = pd.DataFrame([final], columns=col)
+    input_data = data_unseen[['age', 'bmi']].values
     print(int_features)
     print(final)
-    prediction=model.predict(input_data)
-    output=round(prediction[0],2)
-    return render_template('home.html',pred='Expected Bill will be {}'.format(output))
+    prediction = model.predict(input_data)
+    output = round(prediction[0], 2)
+    return render_template('home.html', pred='Expected Bill will be {}'.format(output))
 
-@app.route('/predict_api',methods=['POST'])
+
+@app.route('/predict_api', methods=['POST'])
 def predict_api():
     '''
     For direct API calls trought request
     '''
     data = request.get_json(force=True)
     data_unseen = pd.DataFrame([data])
-    cols=[0,2]
-    input_data=data_unseen[data_unseen.columns[cols]]
+    cols = [0, 2]
+    input_data = data_unseen[data_unseen.columns[cols]]
     prediction = model.predict(input_data)
-    output = round(prediction[0],2)
+    output = round(prediction[0], 2)
     return jsonify(output)
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug = True)
+    app.run(host='0.0.0.0', debug=True)
