264ad2cc8df7bd745dde66ad49cca6c0801f3aef
Anish Shah <anish.shah.emails@gmail.com>
2021-08-20 15:51:30 -0400

    Add Model Training
    Adjust requirements to latest packages to fix issues with wheel building
    Add code for model training and saving
    Add trained saved model to gitignore

--- ML_model_saving.py
+++ ML_model_saving.py
@@ -1,28 +1,31 @@
 # First Task is to build an ML regression model with the data
-import numpy as np
 import pickle
+from pathlib import Path
+
+import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 
 # Step 1: Read the data
 ######Enter Code here#######################
-data=None
+data = pd.read_csv(Path(Path(__file__).parent, "insurance.csv"))
 ##################################################
 
 # Step 2: Extract (bmi and age) features as X and charges as Y
 #######Enter Code here############
-X=None
-y=None
+X = data[["bmi", "age"]]
+y = data["charges"]
 ####################################
 
 # Step 3: Fit a regression model using (X,y), save the model as treemodel.pkl
-####Add code here
-model=None
-pickle.dump(model,open('treemodel.pkl','wb'))
+# Add code here
+model = RandomForestRegressor()
+model.fit(X, y)
+pickle.dump(model, open('treemodel.pkl', 'wb'))
 ###########################################
 
-#Step 4: Test the model for a sample input
-model1=pickle.load(open('treemodel.pkl','rb'))
-x_test=np.array([[15,120]])
+# Step 4: Test the model for a sample input
+model1 = pickle.load(open('treemodel.pkl', 'rb'))
+x_test = np.array([[15, 120]])
 print(x_test)
 print(model1.predict(x_test))
